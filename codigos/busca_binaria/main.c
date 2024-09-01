7c06fb19a439a822300d9feaf50ef17a3e7896f9
Issac Cabral <clidenorissac@gmail.com>
2024-09-01 14:39:23 -0300

    busca binaria

--- codigos/busca_binaria/main.c
+++ codigos/busca_binaria/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int linearSearch(int *L, int length, int target) {
-  for (int i = 0; i < length; i++) {
-    if (L[i] == target) {
-      return i;
+int binarySearch(int *L, int length, int target) {
+  int low = 0, high = length - 1;
+
+  while(low <= high) {
+    int mid = (low + high) / 2;
+
+    if (L[mid] == target) {
+      return mid;
+    }
+
+    if (L[mid] < target) {
+      low = mid + 1; // o elemento está na metade direita
+    } else {
+      high = mid - 1; // o elemento está na metade esquerda
     }
   }
 
@@ -14,7 +24,8 @@ int main() {
   int list[] = {1, 3, 6, 8, 9, 10, 10, 11, 14, 16, 17};
   int listLength = sizeof(list) / sizeof(list[0]);
 
-  
+  int result = binarySearch(list, listLength, 3);
+  printf("Resultado: %d\n", result);
 
   return 0;
 }
