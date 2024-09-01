aa0ec8b1234788e3775050e94391987f0c6e1413
Issac Cabral <clidenorissac@gmail.com>
2024-09-01 09:03:48 -0300

    qst 3

--- codigos/exercicios/listas_encadeadas_e_sequenciais/qst3.c
+++ codigos/exercicios/listas_encadeadas_e_sequenciais/qst3.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+/**
+ * * Sim, podemos reescrever de maneira mais eficiente, caso a lista passada ja esteja ordenada,
+ * * pois só precisamos comparar o elemento atual com o próximo elemento, sem a necessidade de um outro loop interno.
+ * * reduzindo assim, a complexidade.
+ * * NOVA COMPLEXIDADE: O(n) 
+ */
+
+void removeSortedListOccurrences(int *L, int *length) {
+  if (*length <= 1) return;
+
+  int writeIndex = 0;
+
+  for (int i = 0; i < *length - 1; i++) {
+    if (L[i] != L[i + 1]) {
+      L[writeIndex] = L[i];
+      writeIndex++;
+    }
+  }
+
+  L[writeIndex] = L[*length - 1];
+  writeIndex++;
+
+  *length = writeIndex;
+}
+
+void printList(int *L, int length) {
+  for (int i = 0; i < length; i++) {
+    printf("%d ", L[i]);
+  }
+  printf("\n");
+}
+
 int main() {
-  
+  int list[] = {1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 8, 9, 9};
+  int listLength = sizeof(list) / sizeof(list[0]);
+
+  removeSortedListOccurrences(list, &listLength);
+  printList(list, listLength);
 
   return 0;
 }
