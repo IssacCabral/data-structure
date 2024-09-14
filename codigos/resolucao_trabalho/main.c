25ee461ad9e5458a11a8510fa8f721e448d232d3
Issac Cabral <clidenorissac@gmail.com>
2024-09-14 16:10:11 -0300

    remove student

--- codigos/resolucao_trabalho/main.c
+++ codigos/resolucao_trabalho/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct Student {
   char registration[50];
@@ -41,13 +42,27 @@ void insert(StudentList* studentList,char* registration, char* name, float final
   }
 
   Student newStudent = createStudent(registration, name, finalAverage);
-  studentList->data[studentList->length] = newStudent;\
+  studentList->data[studentList->length] = newStudent;
   studentList->length++;
 }
 
-// void remove() {
+bool removeStudent(StudentList* studentList, char* registration) {
+  for (int i = 0; i < studentList->length; i++) {
+    if (strcmp(studentList->data[i].registration, registration) == 0) {
+      printf("Removendo Aluno: %s, Matrícula: %s\n", studentList->data[i].name, studentList->data[i].registration);
 
-// }
+      for (int j = i; j < studentList->length - 1; j++) {
+        studentList->data[j] = studentList->data[j + 1];
+      }
+
+      studentList->length--;
+
+      return true;
+    }
+  }
+
+  return false;
+}
 
 void list(StudentList* studentList) {
   for (int i = 0; i < studentList->length; i++) {
@@ -62,6 +77,13 @@ int main() {
   insert(studentList, "001", "Maria Alencar", 7.89);
   insert(studentList, "002", "Pereira Mathias", 7.89);
   insert(studentList, "003", "Minato Usumaki", 7.89);
+  insert(studentList, "004", "José Arimateia", 7.89);
+  insert(studentList, "005", "Minouri", 7.89);
+  insert(studentList, "006", "Melissa", 7.89);
+
+  list(studentList);
+
+  removeStudent(studentList, "001");
 
   list(studentList);
 
