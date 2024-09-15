361f873069892491d82e5e1e8a18ce2093cdb4ee
Issac Cabral <clidenorissac@gmail.com>
2024-09-15 06:36:43 -0300

    ordenar por matricula

--- codigos/resolucao_trabalho/main.c
+++ codigos/resolucao_trabalho/main.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 typedef struct Student {
-  char registration[50];
+  int registration;
   char name[50];
   float finalAverage;
 } Student;
@@ -15,11 +15,11 @@ typedef struct StudentList {
   int max_capacity;
 } StudentList;
 
-Student createStudent(char* registration, char* name, float finalAverage) {
+Student createStudent(int registration, char* name, float finalAverage) {
   Student newStudent;
 
-  strcpy(newStudent.registration, registration);
   strcpy(newStudent.name, name);
+  newStudent.registration = registration;
   newStudent.finalAverage = finalAverage;
 
   return newStudent;
@@ -35,9 +35,9 @@ StudentList* createStudentList(int max_capacity) {
   return studentList;
 }
 
-void insert(StudentList* studentList,char* registration, char* name, float finalAverage) {
+void insert(StudentList* studentList, int registration, char* name, float finalAverage) {
   if (studentList->length == studentList->max_capacity) {
-    printf("maximum capacity reached");
+    printf("maximum capacity reached\n");
     return;
   }
 
@@ -46,10 +46,10 @@ void insert(StudentList* studentList,char* registration, char* name, float final
   studentList->length++;
 }
 
-bool removeStudent(StudentList* studentList, char* registration) {
+bool removeStudent(StudentList* studentList, int registration) {
   for (int i = 0; i < studentList->length; i++) {
-    if (strcmp(studentList->data[i].registration, registration) == 0) {
-      printf("Removendo Aluno: %s, Matrícula: %s\n", studentList->data[i].name, studentList->data[i].registration);
+    if (studentList->data[i].registration == registration) {
+      printf("Removendo Aluno: %s, Matrícula: %d\n", studentList->data[i].name, studentList->data[i].registration);
 
       for (int j = i; j < studentList->length - 1; j++) {
         studentList->data[j] = studentList->data[j + 1];
@@ -70,13 +70,12 @@ void swap(StudentList* studentList, int i, int j) {
   studentList->data[j] = temp;
 }
 
-int findMinorIndex(StudentList* studentList, int start, int length) {
+int findMinorIndexFinalAverage(StudentList* studentList, int start, int length) {
   int minorIndex = start;
-  Student studentWithMinorFinalAverage = studentList->data[minorIndex];
 
   for (int i = start + 1; i < length; i++) {
     Student student = studentList->data[i];
-    if (student.finalAverage < studentWithMinorFinalAverage.finalAverage) {
+    if (student.finalAverage < studentList->data[minorIndex].finalAverage) {
       minorIndex = i;
     }
   }
@@ -84,43 +83,71 @@ int findMinorIndex(StudentList* studentList, int start, int length) {
   return minorIndex;
 }
 
-void selectionSort(StudentList* studentList, int start) {
+void selectionSortFinalAverage(StudentList* studentList, int start) {
   if (start >= studentList->length - 1) {
     return;
   }
 
-  int minorIndex = findMinorIndex(studentList, start, studentList->length);
+  int minorIndex = findMinorIndexFinalAverage(studentList, start, studentList->length);
 
   if (minorIndex != start) {
     swap(studentList, start, minorIndex);
   }
 
-  selectionSort(studentList, start + 1);
+  selectionSortFinalAverage(studentList, start + 1);
+}
+
+int findMinorIndexRegistration(StudentList* studentList, int start, int length) {
+  int minorIndex = start;
+
+  for (int i = start + 1; i < length; i++) {
+    Student student = studentList->data[i];
+    if (student.registration < studentList->data[minorIndex].registration) {
+      minorIndex = i;
+    }
+  }
+
+  return minorIndex;
+}
+
+void selectionSortRegistration(StudentList* studentList, int start) {
+  if (start >= studentList->length - 1) {
+    return;
+  }
+
+  int minorIndex = findMinorIndexRegistration(studentList, start, studentList->length);
+
+  if (minorIndex != start) {
+    swap(studentList, start, minorIndex);
+  }
+
+  selectionSortRegistration(studentList, start + 1);
 }
 
 void list(StudentList* studentList) {
   for (int i = 0; i < studentList->length; i++) {
     Student student = studentList->data[i];
-    printf("Aluno: %s, Matrícula: %s, Média Final: %.2f\n", student.name, student.registration, student.finalAverage);
+    printf("Aluno: %s, Matrícula: %d, Média Final: %.2f\n", student.name, student.registration, student.finalAverage);
   }
 }
 
 int main() {
   StudentList* studentList = createStudentList(10);
 
-  insert(studentList, "001", "Maria Alencar", 10);
-  insert(studentList, "002", "Pereira Mathias", 1.56);
-  insert(studentList, "003", "Minato Usumaki", 5.78);
-  insert(studentList, "004", "José Arimateia", 2.90);
-  insert(studentList, "005", "Minouri", 2.91);
-  insert(studentList, "006", "Melissa", 1.34);
+  insert(studentList, 4, "Maria Alencar", 10);
+  insert(studentList, 2, "Pereira Mathias", 1.56);
+  insert(studentList, 6, "Minato Usumaki", 5.78);
+  insert(studentList, 1, "José Arimateia", 2.90);
+  insert(studentList, 5, "Minouri", 2.91);
+  insert(studentList, 3, "Melissa", 1.34);
 
   // list(studentList);
 
-  removeStudent(studentList, "001");
+  // removeStudent(studentList, 1);
   // list(studentList);
 
-  selectionSort(studentList, 0);
+  selectionSortFinalAverage(studentList, 0);
+  // selectionSortRegistration(studentList, 0);
   list(studentList);
 
   free(studentList->data);
